8f756880d05646845ba02021d75acd8c77ea569e
Iana Sophia <sophialarge57@gmail.com>
2026-06-30 10:45:54 -0500

    add minimice and close

--- src/components/windowbar.jsx
+++ src/components/windowbar.jsx
@@ -1,5 +1,14 @@
 import "./component.css";
 
 export default function Windowbar() {
-  return <div className="titlebar"></div>;
+  return (
+    <div className="titlebar">
+      <div data-tauri-drag-region className="titlebar">
+        <div className="titlebar-controls">
+          <button id="titlebar-minimize">—</button>
+          <button id="titlebar-close">✕</button>
+        </div>
+      </div>
+    </div>
+  );
 }
