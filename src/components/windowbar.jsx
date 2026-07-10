68e42760a637cc850e5254672693440080f41826
Iana Sophia <sophialarge57@gmail.com>
2026-07-10 13:36:13 -0500

    Feat: cambio de iconos :3

--- src/components/windowbar.jsx
+++ src/components/windowbar.jsx
@@ -1,12 +1,23 @@
 import "./component.css";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+
+const appWindow = getCurrentWindow();
 
 export default function Windowbar() {
   return (
     <div className="titlebar" data-tauri-drag-region>
       <span className="titlebar-title">:3</span>
       <div className="titlebar-controls">
-        <button id="titlebar-minimize" className="titlebar-btn"></button>
-        <button id="titlebar-close" className="titlebar-btn"></button>
+        <button
+          id="titlebar-minimize"
+          className="titlebar-btn"
+          onClick={() => appWindow.minimize()}
+        ></button>
+        <button
+          id="titlebar-close"
+          className="titlebar-btn"
+          onClick={() => appWindow.close()}
+        ></button>
       </div>
     </div>
   );
